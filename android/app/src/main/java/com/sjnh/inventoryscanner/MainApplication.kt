84d8759b702d556d3513b0f691c19082f38cc218
restoshammy <kaurinovich@gmail.com>
2025-08-27 00:27:08 +0100

    Fix MainApplication.kt to use plain ReactNativeHost without ReactHost or expo wrappers

--- android/app/src/main/java/com/sjnh/inventoryscanner/MainApplication.kt
+++ android/app/src/main/java/com/sjnh/inventoryscanner/MainApplication.kt
@@ -6,7 +6,6 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
@@ -14,32 +13,34 @@ import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-    override fun getPackages(): List<ReactPackage> {
-      val packages = PackageList(this).packages
-      // Add manual packages here if needed
-      return packages
+  override val reactNativeHost: ReactNativeHost =
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> {
+        val packages = PackageList(this).packages
+        // Manually add non-autolinked packages here if needed
+        return packages
+      }
+
+      override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
     }
 
-    override fun getJSMainModuleName(): String = "index"
-    override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-    override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-    override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-  }
-
-  override val reactHost: ReactHost
-    get() = reactNativeHost
+  override fun getReactNativeHost(): ReactNativeHost = reactNativeHost
 
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // Load native entry point for New Architecture
       load()
     }
   }
 
   override fun onConfigurationChanged(newConfig: Configuration) {
     super.onConfigurationChanged(newConfig)
-    // no expo ApplicationLifecycleDispatcher anymore
   }
 }
